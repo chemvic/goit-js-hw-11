bf5428436ce3878afd89ff82d01c14201d99da63
chemvic <chemvic79@gmail.com>
2023-03-06 16:38:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
-import axios from 'axios';
-// const axios = require('axios').default;
+// import axios from 'axios';
+const axios = require('axios').default;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const BASE_URL = "https://pixabay.com/api/";
@@ -13,62 +13,69 @@ formEL.addEventListener('submit', onSubmite);
 function onSubmite(event) {
     event.preventDefault();
     const imagesForSearch = event.currentTarget.elements.searchQuery.value;
-    console.log(imagesForSearch);
-
-//     fetchImages(imagesForSearch)
-//         .then(response=>console.log(response.json()))
-//         // .catch(console.log(error))
-// ;
-
-    axios.get("https://pixabay.com/api/?key=34144660-7b9b8b2468352e1d4cb8415b4&q=dog", {
+    
+    fetchImages(imagesForSearch)
+        .then(response => console.log(response));
        
-        params: {
-            // key:"34144660-7b9b8b2468352e1d4cb8415b4",
-            // q: '${ imagesForSearch }',
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: 'true',
-            page: '1',
-            per_page:'40',
-        },
-         headers: {
-            "Content-Type": 'aplication/JSON',
-            Authorization: KEY,
-        }
-    },
-    ).then(response => console.log(response.data))
-        .catch(error => console.error(error));
 
-
+    
 }
 
-//     function fetchImages(imagesForSearch) {
 
-//         const options = {
-//     //   method:'GET',
-//      parameters: {
-//             // key:"34144660-7b9b8b2468352e1d4cb8415b4",
-//             // q: '${ imagesForSearch }',
-//             image_type: 'photo',
-//             orientation: 'horizontal',
-//             safesearch: 'true',
-//             page: '1',
-//             per_page:'40',
-//         },
-//          headers: {
-//             "Content-Type": 'aplication/JSON',
+async function fetchImages(imagesForSearch) {
+    try {
+        return await axios.get(`${BASE_URL}?key=${KEY}&q=${imagesForSearch}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`)
+            console.log(response.data);
+        }
+         catch (error) {
+    console.error(error);
+  }
+
+   }    
+
+
+    
+     
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+      //  async   function fetchImages(imagesForSearch) {
+         // return await fetch(`${BASE_URL}?key=${KEY}&q=${imagesForSearch}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`)
+        //     .then(response => {
+        //         if (!response.ok) {
+        //             throw new Error(response.status);
+               
+        //         }; 
+        //         return response.json();
+        //     });   
+            
+
+
+
+
+       //     const options = {
+    //   method:'GET',
+    //  parameters: {
+    //         key:KEY,
+    //         q: '${ imagesForSearch }',
+    //         image_type: 'photo',
+    //         orientation: 'horizontal',
+    //         : 'true',
+    //         : '1',
+    //         :'40',
+    //     },
+    //      headers: {
+    //         "Content-Type": 'aplication/JSON',
            
-//         }
+        // }
 // }
 
-//         return fetch('https://pixabay.com/api/?key=34144660-7b9b8b2468352e1d4cb8415b4&q=dog')
-//             .then(response => {
-//                 if (!response.ok) {
-//                     throw new Error(response.status);
-               
-//                 }; 
-//                 return response.json();
-//             });   
-            
-//     }
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
 
