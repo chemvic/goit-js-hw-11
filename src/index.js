f931eb9c114466275f6c3cc63e1a7abccd4779a7
chemvic <chemvic79@gmail.com>
2023-03-10 18:10:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
-// import axios from 'axios';
+import axios from 'axios';
 import throttle  from 'lodash.throttle';
-const axios = require('axios').default;
+// const axios = require('axios').default;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox"; 
 import "simplelightbox/dist/simple-lightbox.min.css";
